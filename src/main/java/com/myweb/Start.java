cc5c38cc98b456d5db3acc1015e4e31c6a86b79f
wangyongst <wangyongst@gmail.com>
2018-05-16 15:49:35 +0800

    office

--- src/main/java/com/myweb/Start.java
+++ src/main/java/com/myweb/Start.java
@@ -1,4 +1,9 @@
 package com.myweb;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
@@ -6,6 +11,22 @@ import java.util.Map;
 
 public class Start {
     public static void main(String[] args) throws Exception {
+//  WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://baidu.com");
+        WebElement input = driver.findElement(By.xpath(".//*[@id='kw']"));
+        CharSequence[] cs = new CharSequence[1];
+        cs[0]="安居客";
+        input.sendKeys(cs);
 
+        WebElement btn = driver.findElement(By.xpath(".//*[@id='su']"));
+        btn.click();
+        // WebElement btn1 = driver.findElement(By.xpath(".//*[@id='w-75cn8k']/div/h2/a[1]"));
+        //btn1.click();
+        System.out.println("Page title is:"+driver.getTitle());
+        //Sleep(2000);
+        driver.close();
     }
+}
 }
